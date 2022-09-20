263e39ea960bb0d9e3532d2a3622802e13bccf75
biraneves <ubirajara.neves@usp.br>
2022-09-20 08:05:00 -0300

    Implementação usando TDD.

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,12 +1,28 @@
 import Home from '@/pages/index';
 
-const sum = (a: number, b: number) => {
-    return a + b;
+const capitalizeWord = (word: string) => {
+    const firstLetter = word[0].toLocaleUpperCase();
+    const otherLetters = word.substring(1);
+
+    return `${firstLetter}${otherLetters}`;
 };
 
-describe('Simple operations', () => {
-    test('should return 4 from 2 + 2', () => {
-        expect(sum(2, 2)).toBe(4);
+const capitalize = (text: string) => {
+    if (text === '') return '';
+
+    return text.split(' ').map(capitalizeWord).join(' ');
+};
+
+describe('Sanity of formatter', () => {
+    test('Should do nothing for empty entry', () => {
+        expect(capitalize('')).toBe('');
     });
-    test('should multiply', () => {});
+
+    test('Should return Bira for bira', () => {
+        expect(capitalize('bira')).toBe('Bira');
+    });
+
+    test('Should return Bira Neves for bira neves', () => {
+        expect(capitalize('bira neves')).toBe('Bira Neves');
+    });
 });
